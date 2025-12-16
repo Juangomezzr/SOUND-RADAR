4479a4f730730d0dd61ebabcf514415fc39df81a
Juangomezzr <jugorey@hotmail.com>
2025-12-16 13:46:31 +0200

    Added music list and listening now

--- sound_radar/lib/screens/map_screen.dart
+++ sound_radar/lib/screens/map_screen.dart
@@ -37,10 +37,28 @@ class _MapScreenState extends State<MapScreen> {
       avatarColorHex: '#A855F7',
       instagram: 'https://instagram.com/purple',
       xLink: 'https://x.com/purple',
-      lastSongs: List.generate(
-        8,
-        (i) => Song(title: 'Song $i', artist: 'Artist $i'),
-      ),
+      lastSongs: [
+                    Song(title: 'Midnight City', artist: 'M83'),
+                    Song(title: 'After Dark', artist: 'Mr.Kitty'),
+                    Song(title: 'Blinding Lights', artist: 'The Weeknd'),
+                    Song(title: 'Electric Feel', artist: 'MGMT'),
+                    Song(title: 'Do I Wanna Know?', artist: 'Arctic Monkeys'),
+                    Song(title: 'Starboy', artist: 'The Weeknd'),
+                    Song(title: 'Sunset Lover', artist: 'Petit Biscuit'),
+                    Song(title: 'Nightcall', artist: 'Kavinsky'),
+                    Song(title: 'Feels Like We Only Go Backwards', artist: 'Tame Impala'),
+                    Song(title: 'The Less I Know The Better', artist: 'Tame Impala'),
+                    Song(title: 'Sweater Weather', artist: 'The Neighbourhood'),
+                    Song(title: 'Somebody Else', artist: 'The 1975'),
+                    Song(title: 'Kids', artist: 'MGMT'),
+                    Song(title: 'Riptide', artist: 'Vance Joy'),
+                    Song(title: 'Cold Heart', artist: 'Elton John & Dua Lipa'),
+                    Song(title: 'Levitating', artist: 'Dua Lipa'),
+                    Song(title: '505', artist: 'Arctic Monkeys'),
+                    Song(title: 'Time', artist: 'Hans Zimmer'),
+                    Song(title: 'Intro', artist: 'The xx'),
+                    Song(title: 'A Moment Apart', artist: 'ODESZA'),
+                  ],
     );
 
     _initLocation();
@@ -89,7 +107,26 @@ class _MapScreenState extends State<MapScreen> {
           avatarColorHex: '#7C3AED',
           instagram: 'https://instagram.com/alex',
           xLink: 'https://x.com/alex',
-          lastSongs: [Song(title: 'As It Was', artist: 'Harry Styles')],
+          lastSongs: [Song(title: 'Levitating', artist: 'Dua Lipa'),
+                      Song(title: 'Don’t Start Now', artist: 'Dua Lipa'),
+                      Song(title: 'Style', artist: 'Taylor Swift'),
+                      Song(title: 'Cruel Summer', artist: 'Taylor Swift'),
+                      Song(title: 'Happier Than Ever', artist: 'Billie Eilish'),
+                      Song(title: 'Kill Bill', artist: 'SZA'),
+                      Song(title: 'Good 4 U', artist: 'Olivia Rodrigo'),
+                      Song(title: 'Anti-Hero', artist: 'Taylor Swift'),
+                      Song(title: 'Dance Monkey', artist: 'Tones and I'),
+                      Song(title: 'Titanium', artist: 'David Guetta'),
+                      Song(title: 'Blinding Lights', artist: 'The Weeknd'),
+                      Song(title: 'Flowers', artist: 'Miley Cyrus'),
+                      Song(title: 'Shake It Off', artist: 'Taylor Swift'),
+                      Song(title: 'Watermelon Sugar', artist: 'Harry Styles'),
+                      Song(title: 'Bad Guy', artist: 'Billie Eilish'),
+                      Song(title: 'Someone You Loved', artist: 'Lewis Capaldi'),
+                      Song(title: 'Drivers License', artist: 'Olivia Rodrigo'),
+                      Song(title: 'Halo', artist: 'Beyoncé'),
+                      Song(title: 'Firework', artist: 'Katy Perry'),
+                      Song(title: 'Blank Space', artist: 'Taylor Swift'),],
         ),
         User(
           id: 'u2',
@@ -98,7 +135,26 @@ class _MapScreenState extends State<MapScreen> {
           avatarColorHex: '#22C55E',
           instagram: 'https://instagram.com/sara',
           xLink: 'https://x.com/sara',
-          lastSongs: [Song(title: 'Levitating', artist: 'Dua Lipa')],
+          lastSongs: [Song(title: 'Lose Yourself', artist: 'Eminem'),
+                      Song(title: 'HUMBLE.', artist: 'Kendrick Lamar'),
+                      Song(title: 'SICKO MODE', artist: 'Travis Scott'),
+                      Song(title: 'God’s Plan', artist: 'Drake'),
+                      Song(title: 'Stronger', artist: 'Kanye West'),
+                      Song(title: 'Industry Baby', artist: 'Lil Nas X'),
+                      Song(title: 'Lucid Dreams', artist: 'Juice WRLD'),
+                      Song(title: 'No Role Modelz', artist: 'J. Cole'),
+                      Song(title: 'Mask Off', artist: 'Future'),
+                      Song(title: 'Still D.R.E.', artist: 'Dr. Dre'),
+                      Song(title: 'POWER', artist: 'Kanye West'),
+                      Song(title: 'Alright', artist: 'Kendrick Lamar'),
+                      Song(title: 'Goosebumps', artist: 'Travis Scott'),
+                      Song(title: 'DNA.', artist: 'Kendrick Lamar'),
+                      Song(title: 'Stan', artist: 'Eminem'),
+                      Song(title: 'Hate It Or Love It', artist: 'The Game'),
+                      Song(title: 'Many Men', artist: '50 Cent'),
+                      Song(title: 'In Da Club', artist: '50 Cent'),
+                      Song(title: 'Still Not a Player', artist: 'Big Pun'),
+                      Song(title: 'Shook Ones, Pt. II', artist: 'Mobb Deep'),],
         ),
         User(
           id: 'u3',
@@ -107,8 +163,27 @@ class _MapScreenState extends State<MapScreen> {
           avatarColorHex: '#EF4444',
           instagram: 'https://instagram.com/marco',
           xLink: 'https://x.com/marco',
-          lastSongs: [Song(title: 'Lose Yourself', artist: 'Eminem')],
-        ),
+          lastSongs: [ Song(title: 'As It Was', artist: 'Harry Styles'),
+                        Song(title: 'Blinding Lights', artist: 'The Weeknd'),
+                        Song(title: 'Levitating', artist: 'Dua Lipa'),
+                        Song(title: 'Sunflower', artist: 'Post Malone'),
+                        Song(title: 'Heat Waves', artist: 'Glass Animals'),
+                        Song(title: 'bad guy', artist: 'Billie Eilish'),
+                        Song(title: 'Starboy', artist: 'The Weeknd'),
+                        Song(title: 'Believer', artist: 'Imagine Dragons'),
+                        Song(title: 'Shivers', artist: 'Ed Sheeran'),
+                        Song(title: 'Flowers', artist: 'Miley Cyrus'),
+                        Song(title: 'Watermelon Sugar', artist: 'Harry Styles'),
+                        Song(title: 'Peaches', artist: 'Justin Bieber'),
+                        Song(title: 'Can’t Feel My Face', artist: 'The Weeknd'),
+                        Song(title: 'Senorita', artist: 'Shawn Mendes'),
+                        Song(title: 'Stay', artist: 'The Kid LAROI'),
+                        Song(title: 'Closer', artist: 'The Chainsmokers'),
+                        Song(title: 'Cheap Thrills', artist: 'Sia'),
+                        Song(title: 'Circles', artist: 'Post Malone'),
+                        Song(title: 'Don’t Start Now', artist: 'Dua Lipa'),
+                        Song(title: 'Perfect', artist: 'Ed Sheeran'),],
+                              ),
       ]);
 
     final positions = _generateNearbyPoints(my, _nearbyUsers.length);
@@ -166,30 +241,40 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   void _showUserPopup(User user) {
-    final song = user.lastSongs.isNotEmpty ? user.lastSongs.first : null;
+  final song = user.lastSongs.isNotEmpty ? user.lastSongs.first : null;
 
-    showModalBottomSheet(
-      context: context,
-      backgroundColor: Colors.transparent,
-      builder: (_) {
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+  showDialog(
+    context: context,
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.4),
+    builder: (_) {
+      return Center(
+        child: Material(
+          color: Colors.transparent,
           child: Container(
-            padding: const EdgeInsets.all(16),
+            width: MediaQuery.of(context).size.width * 0.85,
+            padding: const EdgeInsets.all(18),
             decoration: BoxDecoration(
-              color: const Color(0xFF3F3F46),
-              borderRadius: BorderRadius.circular(18),
+              color: const Color(0xFF3A3A3D), // gris oscuro más claro
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 20,
+                  offset: Offset(0, 10),
+                ),
+              ],
             ),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                // Username
+                // USERNAME
                 Container(
                   width: double.infinity,
-                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  padding: const EdgeInsets.symmetric(vertical: 12),
                   decoration: BoxDecoration(
-                    color: const Color(0xFF27272A),
-                    borderRadius: BorderRadius.circular(12),
+                    color: const Color(0xFF2F2F33),
+                    borderRadius: BorderRadius.circular(14),
                   ),
                   child: Center(
                     child: Text(
@@ -197,41 +282,57 @@ class _MapScreenState extends State<MapScreen> {
                       style: const TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
                     ),
                   ),
                 ),
-                const SizedBox(height: 12),
 
-                // Song / Artist
+                const SizedBox(height: 14),
+
+                // SONG
                 Container(
                   width: double.infinity,
-                  padding: const EdgeInsets.all(12),
+                  padding: const EdgeInsets.all(14),
                   decoration: BoxDecoration(
-                    color: const Color(0xFF27272A),
-                    borderRadius: BorderRadius.circular(12),
+                    color: const Color(0xFF2F2F33),
+                    borderRadius: BorderRadius.circular(14),
                   ),
                   child: Column(
                     children: [
-                      Text(song?.title ?? 'Song Name',
-                          style: const TextStyle(fontSize: 16)),
+                      Text(
+                        song?.title ?? 'Song',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
                       const SizedBox(height: 6),
-                      Text(song?.artist ?? 'Artist',
-                          style: const TextStyle(color: Colors.white70)),
+                      Text(
+                        song?.artist ?? 'Artist',
+                        style: const TextStyle(
+                          color: Colors.white70,
+                        ),
+                      ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 12),
 
-                // Go to user
+                const SizedBox(height: 18),
+
+                // BOTÓN GO TO USER (destacado)
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF27272A),
+                      backgroundColor:
+                          const Color(0xFF4F46E5), // acento sutil
                       foregroundColor: Colors.white,
+                      elevation: 6, // sombra ligera
+                      shadowColor: Colors.black54,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+                        borderRadius: BorderRadius.circular(14),
                       ),
                       padding: const EdgeInsets.symmetric(vertical: 14),
                     ),
@@ -243,16 +344,21 @@ class _MapScreenState extends State<MapScreen> {
                         ),
                       );
                     },
-                    child: const Text('Go To User'),
+                    child: const Text(
+                      'Go To User',
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
                 ),
               ],
             ),
           ),
-        );
-      },
-    );
-  }
+        ),
+      );
+    },
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
